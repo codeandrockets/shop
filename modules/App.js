dbd28ea9a59d1c8a8c7c7217cf6ace539cf26e2a
Steven Jay <stevenjasionowicz@gmail.com>
2016-03-30 15:27:39 +1300

    Add clean URLs

--- modules/App.js
+++ modules/App.js
@@ -7,6 +7,7 @@ export default React.createClass({
     return <div>
     	<h1>Vitalife</h1>
     	<ul>
+            <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
     		<li><NavLink to="/about" activeClassName="active">About</NavLink></li>
     		<li><NavLink to="/categories" activeClassName="active">Categories</NavLink></li>
     	</ul>
