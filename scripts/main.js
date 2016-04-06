7095eb1b3963628ba94a0b5877d1f681a942bff4
Steven Jay <stevenjasionowicz@gmail.com>
2016-04-06 15:14:19 +1200

    Add main components and routes

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,90 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//****************   App   *************************//
+//Load in the router
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+//Require Helpers
+var helper = require('./helpers');
+
+//****************   App   *****************************************//
 
 var App = React.createClass({
 
 	render : function() {
 		return (
 			<div>
-				<Header />
-				<Order />
-				<Inventory />
+				<Products />
 			</div>
 		)
 	}
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+//****************   Products   ****************************************//
+
+var Products = React.createClass({
+
+	render : function() {
+		return (
+			<p>Products</p>
+		)
+	}
+});
+
+//****************   Cart   *******************************************//
+
+var Cart = React.createClass({
+	
+	render : function() {
+		return (
+			<p>Cart</p>
+		)
+	}
+});
+
+//****************   Inventory   *************************************//
+
+var Inventory = React.createClass({
+	
+	render : function() {
+		return (
+			<p>Inventory</p>
+		)
+	}
+});
+
+//****************   404 Page ****************************************//
+
+var PageNotFound = React.createClass({
+
+	render : function() {
+		return (
+			<p>Page Not Found</p>
+		)
+	}
+
+});
+
+//****************   Routes   ****************************************//
+
+var routes = (
+	<Router history={createBrowserHistory()}>
+		<Route path="/" component={App}/>
+		<Route path="/cart" component={Cart}/>
+		<Route path="/inventory" component={Inventory}/>
+		<Route path="/*" component={PageNotFound}/>
+	</Router>
+)
+
+//****************   Render to Application   *************************//
+
+ReactDOM.render(routes, document.querySelector('#main'));
+
+
+
+
+
